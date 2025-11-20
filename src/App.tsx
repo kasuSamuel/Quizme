37a8614aa885cfa876a0f7aa0f7604b44b8325a2
Samuel kasu <kasusamuel17@gmail.com>
2025-11-20 09:52:22 +0000

    feat(app): format project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import Home from "./components/home"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+
+import Home from './components/home'
+import Questions from './components/questions'
+import Results from './components/results'
+import LoadingScreen from './components/loading'
+
+import './App.css'
 
 const App = () => {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000)
+  }, [])
+
+  if (loading) return <LoadingScreen />
 
   return (
-    <Home />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/questions/:topic" element={<Questions />} />
+        <Route path="/results" element={<Results />} />
+      </Routes>
+    </Router>
   )
 }
 
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,10 +1,108 @@
-const Home = () => {
-  return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the main landing page of our application.</p>
-    </div>
-  );
-};
+import styled, { keyframes } from 'styled-components'
+import Tab from './tab'
+import { useNavigate } from 'react-router-dom'
+import LoadingScreen from './loading'
+import { useGetCategoriesQuery } from '../store/GameSlice'
 
-export default Home;
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(10px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`
+
+const bounce = keyframes`
+  0%, 20%, 50%, 80%, 100% { transform: translateY(0); }
+  40% { transform: translateY(-30px); }
+  60% { transform: translateY(-15px); }
+`
+
+const Background = styled.div`
+  width: 60%;
+  display: flex;
+  gap: 40px;
+  justify-content: center;
+  align-items: center;
+  color: #1b1b1b;
+`
+
+const Title = styled.h1`
+  font-size: 5rem;
+  font-weight: 900;
+  margin: 0;
+  color: #1a202c;
+  text-shadow:
+    -2px -2px 0px #60a5fa,
+    2px 2px 0px #a78bfa;
+  font-family: 'Fredoka One';
+  display: inline-block;
+  animation: ${bounce} 2s infinite;
+`
+
+const LeftSide = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  width: 50%;
+`
+
+const RightSide = styled.div`
+  width: 50%;
+`
+
+const Subtitle = styled.p`
+  font-size: 0.9rem;
+  color: #8b8b8bff;
+  animation: ${fadeIn} 1.2s ease-in-out;
+  font-style: italic;
+`
+
+const TabList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  margin-top: 2rem;
+`
+
+const Home = () => {
+  const { data: category = [], isLoading } = useGetCategoriesQuery('')
+  const navigate = useNavigate()
+
+  if (isLoading) return <LoadingScreen />
+
+
+  const handleTabClick = (title: string) => {
+    navigate(`/questions/${title}`)
+  }
+
+  return (
+    <Background>
+      <LeftSide>
+        <Title>Quiz-me</Title>
+        <Subtitle>Discover your potential with Quiz-me!</Subtitle>
+      </LeftSide>
+
+      <RightSide>
+        <TabList>
+          {category.map((tab, index) => (
+            <Tab
+              onClicked={() => handleTabClick(tab?.title ?? '')}
+              key={index}
+              type="area"
+title={tab.title.toUpperCase()}
+              totalQuestions={tab.totalQuestions}
+              imgSrc={tab.imgSrc}
+            />
+          ))}
+        </TabList>
+      </RightSide>
+    </Background>
+  )
+}
+
+export default Home
